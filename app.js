a34538fe8dc9074efec75a3b8658c5ac29cb1a7a
shuxter <simon.huxter@gmail.com>
2021-01-28 00:37:36 -0300

    committing before cleaning up the structure of the css and html

--- app.js
+++ app.js
@@ -16,3 +16,23 @@ app.listen(PORT, () => {
 app.get("/", (req, res) => {
 	res.sendFile(path.resolve(__dirname, "./views/home.html"));
 });
+
+app.post("/", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+
+app.get("/register", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
+
+app.post("/register", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
+
+app.post("/login", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
